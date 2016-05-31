5a2073ae3c6459ecd892677bd8fabfba2caa9d43
G. Grau <guigrpa@gmail.com>
2016-05-31 15:02:19 +0200

    v1.3.0

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 // Basic config
 // ===============================================
 const NAME = 'extract-docs';
-const VERSION = '1.2.0';
+const VERSION = '1.3.0';
 const DESCRIPTION = 'A simple CLI tool to gather documentation from JS code';
 const KEYWORDS = ['tools', 'docs'];
 
